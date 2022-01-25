3fe319f2213300a300678364e06ac2e4ba763630
Radik <RDKJaz@gmail.com>
2022-01-25 22:32:30 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Radik Gazetdinov on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
