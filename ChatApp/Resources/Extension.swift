055ef82d2f955a811c46e1a6d4b3b0c26faedc35
Radik <RDKJaz@gmail.com>
2022-02-01 23:17:53 +0300

    Создание экранов авторизации и регистрации

---
+++ ChatApp/Resources/Extension.swift
@@ -0,0 +1,36 @@
+//
+//  Extension.swift
+//  ChatApp
+//
+//  Created by Radik Gazetdinov on 26.01.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.height + self.frame.origin.x
+    }
+}
